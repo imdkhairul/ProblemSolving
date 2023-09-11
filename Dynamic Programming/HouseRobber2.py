a96a3fe96cc83cf9734b996ba2630ba596749a41
imdkhairul <imdkhairul@gmail.com>
2023-09-12 04:34:35 +0600

    Create HouseRobber2.py

---
+++ Dynamic Programming/HouseRobber2.py
@@ -0,0 +1,12 @@
+def rob(nums):
+    return max(nums[0],helper(nums[1:]),helper(nums[:-1]))
+
+def helper(nums):
+    rob1, rob2 = 0, 0
+    for n in nums:
+        newrob = max((rob1 + n) ,rob2)
+        rob2 = rob1
+        rob1 = newrob
+    return rob2
+
+print(rob([2,3,2]))
