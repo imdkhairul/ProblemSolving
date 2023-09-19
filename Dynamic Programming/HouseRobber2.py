4f7f1c28f5feec187c71250ad41fc1100bb748cb
imdkhairul <imdkhairul@gmail.com>
2023-09-19 07:20:02 +0600

    Update HouseRobber2.py

--- Dynamic Programming/HouseRobber2.py
+++ Dynamic Programming/HouseRobber2.py
@@ -5,8 +5,20 @@ def helper(nums):
     rob1, rob2 = 0, 0
     for n in nums:
         newrob = max((rob1 + n) ,rob2)
-        rob2 = rob1
-        rob1 = newrob
+        rob1 = rob2
+        rob2 = newrob
     return rob2
 
-print(rob([2,3,2]))
+def numDecodings(str):
+    dp = {len(str):1}
+    for i in range(len(str) - 1, -1, -1):
+        if str[i] == "0":
+            dp[i] = 0
+        else:
+            dp[i] = dp[i+1]
+        
+        if i + 1 < len(str) and str[i] == "1" or str[i] == "2" and str[i+1] in "0123456":
+            dp[i] += dp[i + 2]
+    return dp
+
+print(numDecodings("1234"))
